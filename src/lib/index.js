8985fbcb257fed05883bd5c6ad866614264e7f37
Ed Lee <jonexiu@gmail.com>
2018-09-11 21:50:05 +0800

    Add FamilyMark

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,5 +4,6 @@ import Loader from './Loader'
 import Nothing from './Nothing'
 import Prompt from './Prompt'
 import Face from './Face'
+import FamilyMark from './FamilyMark'
 
-export { Button, Icon, Loader, Nothing, Prompt, Face }
+export { Button, Icon, Loader, Nothing, Prompt, Face, FamilyMark }
